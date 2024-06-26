9930884818c0236dfe8d4984b13cc6baef8c282e
makoto hata <makotohata0429@gmail.com>
2024-06-26 22:42:04 +0900

    file upload

---
+++ app/http/upload/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	engine:= gin.Default()
+	engine.POST("/upload", func(c *gin.Context) {
+		file, header, err := c.Request.FormFile("image")
+		if err != nil {
+			c.String(http.StatusBadRequest, "Bad request")
+			return
+		}
+		fileName := header.Filename
+		dir, _ := os.Getwd()
+		out, err := os.Create(dir+"/http/upload/images/"+fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+		_, err = io.Copy(out, file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, gin.H {
+			"status": "ok",
+			"dir": dir,
+			"fileName": fileName,
+		})
+	})
+	engine.Run(":3000")
+}
